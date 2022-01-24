acbc75fb9fbb5110564adf4c4c8f9116b5410cea
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-01-24 15:59:42 -0800

    iterate through slides next button
    remove visibility, increment if not at end of array, else reset to beginning

--- carousel.js
+++ carousel.js
@@ -7,9 +7,19 @@ document.getElementById("carousel-button-prev").addEventListener("click", moveTo
 document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide);
 
 function moveToNextSlide(){
-
+    console.log("next slide");
+    slides[slidePosition].classList.remove("carousel-item-visible");
+        // check if at end of slides
+    if(slidePosition == totalSlides - 1){
+        console.log("End of slides");
+        // reset slide position back to beginning
+        slidePosition = 0;
+    } else{
+        slidePosition++;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
 
 function moveToPrevSlide(){
-
+    console.log("prev slide");
 }
